10d509253898f5f1ba56401bd9b0b7ce17694093
cheojorge <cheojorge@hotmail.com>
2022-01-12 21:12:03 -0500

    Poniendo todo junto

--- apoyalo/src/Component/PersonCard.js
+++ apoyalo/src/Component/PersonCard.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 
 class PersonalCards extends Component{
+    constructor(props){
+        super(props);
+        this.state = {pos : this.props.age};
+    }
+    agePlus1 = () => {
+        this.setState({ pos: this.state.pos +1});
+    }
     render(){
         return(
             <>
                 <h1>{this.props.lastName}, {this.props.firstName}</h1>
-                <h3>Age: {this.props.age}</h3>
+                <h3>Age: {this.state.pos}</h3>
                 <h3>Hair Color: {this.props.hairColor}</h3>
+                <button onClick={ this.agePlus1}>Birthday Button for {this.props.firstName} {this.props.lastName}</button>
             </>
         )
     }
